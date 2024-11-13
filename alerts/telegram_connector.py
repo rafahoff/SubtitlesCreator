4f9c705ae638c49e74ec66670742a32691b31f64
Rafael Hoffmann <rafahoff@gmail.com>
2024-11-13 15:57:42 -0300

    Check internet connection

--- alerts/telegram_connector.py
+++ alerts/telegram_connector.py
@@ -1,4 +1,5 @@
 import json
+import requests
 from telegram import Bot
 import constants
 
@@ -14,12 +15,17 @@ CHAT_ID = data['TELEGRAM_CHAT_ID']
 if TOKEN == "" or CHAT_ID == "":
     raise Exception("Token ou Chat ID não configurados. Verifique o arquivo config.json")
     
-
 bot = Bot(token=TOKEN)
 
+def check_internet_connection():
+    try:
+        requests.get('http://www.google.com', timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False
+
 async def send(message: str):
+    if not check_internet_connection():
+        print("Alerta: Sem conexão com a internet. Prosseguindo com a execução.")
     bot = Bot(token=TOKEN)
     await bot.send_message(chat_id=CHAT_ID, text=message)
-
-
-
